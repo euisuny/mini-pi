5fcdf96256144590f9aec1b1927d95b5c92176a1
euisuny <ey222@cornell.edu>
2020-04-28 09:42:58 -0400

    Basic case works, woo

--- eval.ml
+++ eval.ml
@@ -1,5 +1,9 @@
 open Ast
+open Util
+open Printf
 open Hashtbl
+
+let debug = Printf.printf
 
 type action =
 | ASend of var
@@ -11,6 +15,15 @@ type exp =
   | Par of exp * exp
   | Bang of exp
   | Zero
+
+(* Pretty printing expressions. *)
+let rec to_string (e : exp) : string =
+  match e with
+  | Act (c, ASend v, p) -> sprintf "%s <%s>.%s" c v (to_string p)
+  | Act (c, ARecv v, p) -> sprintf "%s (%s).%s" c v (to_string p)
+  | Par (p, p') -> sprintf "%s | %s" (to_string p) (to_string p')
+  | Bang p -> sprintf "! %s" (to_string p)
+  | Zero -> sprintf "0"
 
 let rec translate (p : pexp) : exp =
   match p with
@@ -28,7 +41,6 @@ let rec translate (p : pexp) : exp =
   | PBang p -> Bang (translate p)
   | PZero -> Zero
 
-(* TODO: Implement capture-avoiding substitution. *)
 let rec subst (v : exp) (x : var) (nx : var) : exp =
   match v with
   | Act (chan, act, e1) ->
@@ -62,24 +74,18 @@ let async f = perform (Async f)
 effect Await : 'a promise -> 'a
 let await p = perform (Await p)
 
-(* Message passing primitives. *)
+effect Yield : unit
+let yield () = perform Yield(* Message passing primitives. *)
+
 effect Send : (channel * var) -> unit
 let send c v = perform (Send (c,v))
 
 effect Recv : channel -> var
 let recv x = perform (Recv x)
 
-let send_table : (channel, var) t = Hashtbl.create 30
-let recv_table : (channel, unit) t = Hashtbl.create 30
-
-(* Scheduler: queue of processes. *)
-let q = Queue.create ()
-let enqueue t = Queue.push t q
-let dequeue () =
-  if Queue.is_empty q then ()
-  else Queue.pop q ()
 
 let rec step (e : exp) : exp =
+  debug "Stepping: %s \n" (to_string e);
   match e with
   | Act (c, a, e) -> begin
       match a with
@@ -91,17 +97,26 @@ let rec step (e : exp) : exp =
           step e
     end
   | Par (el, er) ->
-      let pl = async (fun () -> el) in
-      let pr = async (fun () -> er) in
-      let pc = async (fun () -> Par (await pr, await pl)) in
-      step (await pc)
+      let pl = async (fun () -> step el) in
+      let pr = async (fun () -> step er) in
+      Par (await pr, await pl)
   | Bang p ->
-      let pr = async (fun () -> p) in
+      let pr = async (fun () -> step p) in
       let pw = async (fun () -> Bang p) in
       Par (await pr, await pw)
   | v -> v
 
 let run main =
+  let send_table : (channel, var) t = Hashtbl.create 30 in
+  let recv_table : (channel, unit) t = Hashtbl.create 30 in
+
+  (* Scheduler: queue of processes. *)
+  let q = Queue.create () in
+  let enqueue t = Queue.push t q in
+  let dequeue () =
+    if Queue.is_empty q then ()
+    else Queue.pop q () in
+
   let rec fork : 'a. 'a promise -> (unit -> 'a) -> unit =
     fun pr main ->
       match main () with
@@ -111,7 +126,8 @@ let run main =
             | _ -> failwith "impossible"
           in
           List.iter (fun k -> enqueue (fun () -> continue k v)) l;
-          pr := Done v; dequeue ()
+          pr := Done v;
+          dequeue ()
       | effect (Async f) k ->
           let pr = ref (Waiting []) in
           enqueue (fun () -> continue k pr);
@@ -124,9 +140,12 @@ let run main =
               dequeue ()
             end
           end
+      | effect Yield k ->
+          enqueue (continue k);
+          dequeue ()
       | effect (Send (c, v)) k ->
           begin match Hashtbl.find_opt recv_table c with
-          | None -> Hashtbl.add send_table c v; dequeue ()
+          | None -> Hashtbl.add send_table c v; enqueue (continue k); dequeue ()
           | Some r -> continue k ()
           end
       | effect (Recv c) k ->
@@ -135,4 +154,11 @@ let run main =
           | Some r -> continue k r
           end
   in
-  fork (ref (Waiting [])) main
+  fork (ref (Waiting [])) main;
+  Hashtbl.iter (fun k v -> debug "send_table [Key: %s, Value: %s]\n" k v) send_table;
+  Hashtbl.iter (fun k () -> debug "recv_table [Key: %s, Value: ()]\n" k) recv_table
+
+let prog = Par (Act ("hi", ASend "msg", Zero), Act ("hi", ARecv "msg", Zero))
+let foo () = let p = step prog in p; debug "P-result: %s\n" (to_string p); p
+
+let f = run foo
