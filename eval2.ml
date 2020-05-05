d1119aeb100c9536c802e4aa7f857c65998920d9
euisuny <ey222@cornell.edu>
2020-05-05 13:42:51 -0400

    Eval experiment 2

---
+++ eval2.ml
@@ -0,0 +1,22 @@
+open Util
+open Printf
+
+type id = string
+
+(* Core Pict AST *)
+type exp =
+  | Send of id * id
+  | Recv of id * id * exp
+  | BangR of id * id * exp
+  | Par of exp * exp
+  | New of id * exp
+  | Zero
+
+let rec to_string (e : exp) =
+  match e with
+  | Send (c, v) -> sprintf "%s!%s" c v
+  | Recv (c, v, p) -> sprintf "%s?%s = %s" c v (to_string p)
+  | BangR (c, v, p) -> sprintf "%s?*%s = %s" c v (to_string p)
+  | Par (p, p') -> sprintf "%s | %s" (to_string p) (to_string p')
+  | New (v, p) -> sprintf "new %s %s" v (to_string p)
+  | Zero -> sprintf "0"
